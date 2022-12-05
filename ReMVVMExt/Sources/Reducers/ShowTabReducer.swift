28bef06c26fca48a51f1f0f80b920085043ba3fb
Greg Jurzak <greg.jurzak@hyd.agency>
2022-12-05 14:07:00 +0100

    test

--- ReMVVMExt/Sources/Reducers/ShowTabReducer.swift
+++ ReMVVMExt/Sources/Reducers/ShowTabReducer.swift
@@ -28,22 +28,17 @@ struct ShowReducer: Reducer {
         let factory = action.controllerInfo.factory ?? state.factory
         if action.navigationType == state.root.navigationType { //check the type is the same
             stacks = state.root.stacks.map {
-                guard $0.0 == current, $0.1.isEmpty else { return $0 }
-                if action.resetStack {
-                    return ($0.0, [])
-                } else {
-                    return ($0.0, [factory])
+                guard $0.0 == current, $0.1.isEmpty else {
+                    if action.resetStack { return ($0.0, [factory]) }
+                    return $0
                 }
+                return ($0.0, [factory])
 
             }
         } else {
             stacks = action.navigationType.map {
                 guard $0 == current else { return ($0, []) }
-                if action.resetStack {
-                    return ($0, [])
-                } else {
-                    return ($0, [factory])
-                }
+                return ($0, [factory])
             }
         }
         let root = NavigationRoot(current: current, stacks: stacks)
